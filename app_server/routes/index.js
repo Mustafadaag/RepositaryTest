89b4a5fb38c48c62add566bfe8d2f46cacd1f3d2
Mustafadaag <mustafadag966@gmail.com>
2018-11-08 01:31:01 +0300

    son gonderimmmmm

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
+/*get home page*/
 router.get('/',ctrlMekanlar.anaSayfa);
 router.get('/mekan',ctrlMekanlar.mekanBilgisi);
-
 router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
 router.get('/hakkinda', ctrlDigerleri.hakkinda)
-module.exports=router;
+
+module.exports = router;
 
